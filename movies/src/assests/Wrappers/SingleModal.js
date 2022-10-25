974d29d76ae9b702487ec7ffa3aba362d73cc9aa
ronoc123 <98913680+ronoc123@users.noreply.github.com>
2022-10-25 13:44:05 -0500

    final changes

--- movies/src/assests/Wrappers/SingleModal.js
+++ movies/src/assests/Wrappers/SingleModal.js
@@ -10,13 +10,13 @@ const Wrapper = styled.div`
   width: 100%;
   background: rgba(0, 0, 0, 0.3);
   z-index: 4;
+
   /* opacity: 0.2; */
   .info-container {
-    opacity: 1;
     width: 80%;
     max-width: 80rem;
     background: white;
-    height: 70%;
+    min-height: 70%;
     color: black;
     display: grid;
     grid-template-columns: 1fr 2fr;
@@ -42,20 +42,76 @@ const Wrapper = styled.div`
   .text-container {
     padding: 1rem;
   }
+  .heading-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    place-items: center;
+  }
+
+  .rating {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .star {
+    display: grid;
+    align-self: center;
+    justify-self: right;
+    color: #f6ca2a;
+    font-size: 3rem;
+  }
+  .nums {
+    display: grid;
+    align-self: center;
+    /* place-items: center; */
+    font-size: 2rem;
+  }
+  .genres {
+    margin-top: 0.5rem;
+    display: flex;
+    gap: 0.5rem;
+
+    flex-wrap: wrap;
+  }
+  .single-genre {
+    background: #add8e6;
+    border-radius: 1rem;
+    padding: 0.5rem;
+  }
+  .end {
+    margin-top: 5rem;
+    font-size: 1.3rem;
+
+    padding-bottom: 2rem;
+  }
+  .description {
+    margin-top: 1rem;
+  }
+  .release {
+    margin-top: 0.5rem;
+  }
 
   @media screen and (max-width: 1100px) {
     .info-container {
-      height: 45%;
+      min-height: 45%;
     }
   }
 
   @media screen and (max-width: 800px) {
     .info-container {
-      height: 60%;
+      min-height: 60%;
       grid-template-columns: 1fr;
     }
     .image {
       display: none;
+    }
+    .text-title {
+      font-size: 1.5rem;
+    }
+    .star {
+      font-size: 1.8rem;
+    }
+    .nums {
+      font-size: 1.3rem;
     }
   }
 `;
--- movies/src/components/SingleModal.js
+++ movies/src/components/SingleModal.js
@@ -1,12 +1,20 @@
 import Wrapper from "../assests/Wrappers/SingleModal";
 import { AiOutlineCloseSquare } from "react-icons/ai";
 import { img_500, unavailable } from "../config/config";
+import { AiFillStar } from "react-icons/ai";
 
 const SingleModal = ({ setShowModal, singleMovie }) => {
+  function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
+    return x;
+  }
+
   return (
     <Wrapper>
-      <div className="info-container" onClick={() => setShowModal(false)}>
-        <div className="close-icon">
+      <div className="info-container">
+        <div className="close-icon" onClick={() => setShowModal(false)}>
           <AiOutlineCloseSquare />
         </div>
         <img
@@ -19,14 +27,37 @@ const SingleModal = ({ setShowModal, singleMovie }) => {
           className="image"
         />
         <div className="text-container">
-          <h1 className="text-title">
-            {singleMovie.original_title || singleMovie.name}
-          </h1>
+          <div className="heading-container">
+            <h1 className="text-title">
+              {singleMovie.original_title || singleMovie.name}
+            </h1>
+            <div className="rating">
+              <div className="star">
+                <AiFillStar />
+              </div>
+              <div className="nums">
+                {Math.round(singleMovie.vote_average * 10) / 10} / 10
+              </div>
+            </div>
+          </div>
           <div>{singleMovie.release_date}</div>
-          <div>{singleMovie.genres.map((item) => " " + item.name)}</div>
-          <div>{singleMovie.status}</div>
-          <div>{singleMovie.vote_averge}</div>
-          <div>{singleMovie.overview}</div>
+          <div className="genres">
+            {singleMovie.genres.map((item, i) => (
+              <div className="single-genre" key={i}>
+                {item.name}
+              </div>
+            ))}
+          </div>
+          <div className="release">{singleMovie.status}</div>
+          <div className="description">{singleMovie.overview}</div>
+          <div className="end">
+            <div>Budget: ${numberWithCommas(singleMovie.budget)}</div>
+            <div>Revenue: ${numberWithCommas(singleMovie.revenue)}</div>
+            <div>
+              Duration: {Math.floor(singleMovie.runtime / 60)} Hours{" "}
+              {singleMovie.runtime % 60} Minutes
+            </div>
+          </div>
         </div>
       </div>
     </Wrapper>
