e28c8905763fe9effdd7460f5cceeb17d8f685a7
ronoc123 <98913680+ronoc123@users.noreply.github.com>
2023-08-12 18:13:20 -0500

    add user endpoints

--- movie-application/src/main/java/com/movieapp/movieapplication/controller/UserController.java
+++ movie-application/src/main/java/com/movieapp/movieapplication/controller/UserController.java
@@ -4,6 +4,7 @@ import com.movieapp.movieapplication.model.User;
 import com.movieapp.movieapplication.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,6 +24,19 @@ public class UserController {
 
         return user;
     }
+
+    @GetMapping("/api/v1/users")
+    public List<User> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        if (users.isEmpty())
+            throw new RuntimeException("No users Registered");
+
+
+        return users;
+    }
+
+
     @PostMapping("/api/v1/users")
     public User createUser(@RequestBody User user) {
         userRepository.save(user);
--- movie-application/src/main/java/com/movieapp/movieapplication/model/User.java
+++ movie-application/src/main/java/com/movieapp/movieapplication/model/User.java
@@ -25,8 +25,12 @@ public class User {
 
     private LocalDateTime userCreationDate;
 
-    public User(String name, String password, String email) {
+    public User() {
+    }
+
+    public User(Integer id, String name, String password, String email) {
         super();
+        this.id = id;
         this.name = name;
         this.password = password;
         this.email = email;
@@ -35,5 +39,45 @@ public class User {
     @PrePersist
     public void prePersist() {
         userCreationDate = LocalDateTime.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getUserCreationDate() {
+        return userCreationDate;
+    }
+
+    public void setUserCreationDate(LocalDateTime userCreationDate) {
+        this.userCreationDate = userCreationDate;
     }
 }
