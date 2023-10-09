3b24a0cc46871cc744eb3ecf9eec98c239704027
ronoc123 <98913680+ronoc123@users.noreply.github.com>
2023-10-09 16:18:58 -0500

    added user popup modal

--- movies/src/assests/Wrappers/User.js
+++ movies/src/assests/Wrappers/User.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 3fr 2fr;
+  grid-template-columns: 1fr 8fr 5fr;
   margin-bottom: 2rem;
   .img-con {
     display: grid;
@@ -12,6 +12,7 @@ const Wrapper = styled.div`
   img {
     width: 100%;
     height: 100%;
+    border-radius: 50%;
   }
 
   .info-con {
--- movies/src/components/User/UserCard.js
+++ movies/src/components/User/UserCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Wrapper from "../../assests/Wrappers/User";
-
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Modal from "@mui/material/Modal";
 import { FaUserCircle } from "react-icons/fa";
 import { useAppContext } from "../../Context/appContext";
+import ModeIcon from "@mui/icons-material/Mode";
 
 const UserCard = () => {
   const { user } = useAppContext();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <Wrapper>
@@ -39,6 +47,78 @@ const UserCard = () => {
           <span className="title">favorites</span>
         </div>
       </div>
+      <Button
+        onClick={handleOpen}
+        style={{
+          color: "white",
+        }}
+      >
+        Edit{" "}
+        <ModeIcon
+          style={{
+            fontSize: ".8rem",
+          }}
+        />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "1rem",
+            boxShadow: 24,
+            p: 4,
+            background: "white",
+          }}
+        >
+          <div
+            style={{
+              display: "grid",
+              placeItems: "center",
+              padding: "3rem",
+            }}
+          >
+            <TextField
+              id="outlined-multiline-flexible"
+              label="First Name"
+              multiline
+              maxRows={4}
+            />
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Last Name"
+              multiline
+              maxRows={4}
+            />
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Profile Picture"
+              multiline
+              maxRows={4}
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              rows={4}
+              defaultValue="Default Value"
+            />
+          </div>
+        </Box>
+      </Modal>
     </Wrapper>
   );
 };
